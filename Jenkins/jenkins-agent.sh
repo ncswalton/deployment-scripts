c4015372e276619a6e7f478cc0a80327da162002
NickWalton <nicholas.walton@anikasystems.com>
2022-06-02 08:19:26 -0400

    Add sshpass

--- Jenkins/jenkins-agent.sh
+++ Jenkins/jenkins-agent.sh
@@ -6,12 +6,16 @@ sudo yum -y install java-11-openjdk java-11-openjdk-devel
 
 sudo yum -y install maven
 
+sudo yum -y install sshpass
+
 # at this point we have java and maven
 # To do:
 # get the jar file
 # run the jar file (need the secret value and the controller IP for this command)
 # https://github.com/chorrell/install-jenkins-agent/blob/main/install-jenkins-agent.sh
 
-curl -o /home/testUser/agent.jar -Ssl http://$1/jnlpJars/agent.jar
+echo $2 > /home/testUser/creds.txt
 
-echo $1 > /tmp/foo.txt
+curl -o /home/testUser/agent.jar -Ssl http://$1:8080/jnlpJars/agent.jar
+
+sshpass -f "/home/testUser/creds.txt" scp -r testUser@$1:/home/testUser/secret.txt /home/testUser/secret.txt
